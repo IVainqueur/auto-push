f0e38eec69633aedd7ddcb59e84ff77f3313bad5
IVainqueur <ishimvainqueur@gmail.com>
2022-08-06 11:58:47 +0200

    auto-commit-5d2911d9775b4859a8b30d8932d964cc

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # move the main files to a hidden directory in the home folder
 # the hidden directory would be called .auto-push
 
-cp ./auto-push ./main.py ./methods.py ~/.auto-push
+mkdir -p ~/.auto-push && cp ./auto-push ./main.py ./methods.py ~/.auto-push
 
 # Add the directory to PATH
 
