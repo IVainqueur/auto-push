24712cbd0acddbf2f1324e29479bb4f08e146945
IVainqueur <ishimvainqueur@gmail.com>
2022-08-06 12:06:13 +0200

    auto-commit-260de640f4094fff90b5f825862bf8ec

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mkdir -p ~/.auto-push && cp ./auto-push ./main.py ./methods.py ~/.auto-push
 
 # Add the directory to PATH
 
-export PATH=$HOME/.auto-push:$PATH
+export PATH=$PATH:$HOME/.auto-push
 
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
