418ecc0ac6e898f1144e4ba6b19c3ff6acc8ac71
IVainqueur <ishimvainqueur@gmail.com>
2022-08-07 16:55:33 +0200

    auto-commit-4877690b614747f080c5c0d9075fd449

--- win64-installer.py
+++ win64-installer.py
@@ -10,10 +10,12 @@ def is_admin():
 if is_admin():
     cmd = [
         "if not exist \"C:\\Program Files\\auto-push\" mkdir \"C:\\Program Files\\auto-push\"",
-        "copy \".\\dist\\auto-push.exe\" \"C:\\Program Files\\auto-push\""
+        "copy \".\\dist\\auto-push.exe\" \"C:\\Program Files\\auto-push\"",
+        "SETX {0} {1}".format("PATH", (os.environ["PATH"] + "\"C:\\Program Files\\auto-push\""))
     ]
     for command in cmd:
         os.system(command)
+    
 else:
     # Re-run the program with admin rights
     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
