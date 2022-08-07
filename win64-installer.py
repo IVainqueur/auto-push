ed032413d1117eafd4ca03960bbcb4ae26c59530
IVainqueur <ishimvainqueur@gmail.com>
2022-08-07 18:30:51 +0200

    auto-commit-c28998b014334aed96d881ec7b84c2ff

--- win64-installer.py
+++ win64-installer.py
@@ -12,7 +12,7 @@ if is_admin():
         "if not exist \"C:\\Program Files\\auto-push\" mkdir \"C:\\Program Files\\auto-push\"",
         "copy \".\\dist\\auto-push.exe\" \"C:\\Program Files\\auto-push\"",
         "setx {0} \"{1}\"".format("PATH", (os.environ["PATH"] + "\"C:\\Program Files\\auto-push\"")),
-        "msg %username% Auto-Push is has been installed successfully. Final step is to add 'C:\\Program Files\auto-push' to the PATH environment variable"
+        "msg %username% Auto-Push is has been installed successfully. Final step is to add 'C:\\\\Program Files\\auto-push' to the PATH environment variable"
     ]
     for command in cmd:
         os.system(command)
