b48a293a103c456b2c0a45cb00fe916142c608b9
IVainqueur <ishimvainqueur@gmail.com>
2022-08-10 22:22:28 +0200

    auto-commit-7494547b07694de696443b50001a2b6d

--- win64-installer.py
+++ win64-installer.py
@@ -19,7 +19,8 @@ if is_admin():
     cmd = [
         r'''if not exist "C:\\Program Files\auto-push" mkdir "C:\\Program Files\auto-push"''',
         f'''copy "{_path}\\auto-push.exe" "C:\\\\Program Files\\auto-push"''',
-        "msg %username% Auto-Push is has been installed successfully. Final step is to add 'C:\\\\Program Files\\auto-push' to the PATH environment variable"
+        "msg %username% Auto-Push is has been installed successfully. Final step is to add 'C:\\\\Program Files\\auto-push' to the PATH environment variable",
+        r'''"C:\Windows\system32\rundll32.exe" sysdm.cpl,EditEnvironmentVariables'''
     ]
     
     for command in cmd:
