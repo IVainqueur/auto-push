ff024f7617a36785abb51a6f40b2a57f76a1e7dd
IVainqueur <ishimvainqueur@gmail.com>
2022-08-15 00:13:15 +0200

    auto-commit-6df7f27c57604d1a9ae7d7fa70daee2b

--- methods.py
+++ methods.py
@@ -186,7 +186,7 @@ def help():
     print("Note: You can click q anytime to quit\n\n")
 
 def listenForKeys(key):
-    print(colorcode(f"Received {key}", bg="bg-green"))
+    print(colorcode(f"Received {key}", bg="bg-yellow"))
     action = KEYS_WITH_ACTIONS.get(repr(key), None)
     if action:
         action()
