181e829159875f295a7770f7c264ce8d7636f26a
IVainqueur <ishimvainqueur@gmail.com>
2022-08-14 23:25:47 +0200

    auto-commit-3995edc1d74f4aaebe6fe69a949676a3

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ if "--before-mod" and "--before-method" in params.keys():
 
 
 """ Push periodically """
-Timer(5, partial(push, commit_template, dir, branch, interval, beforemethod))
+Timer(10, partial(push, commit_template, dir, branch, interval, beforemethod))
 
 """ Wait for key presses """
 while True:
