c5c4af0fd20baa8d92267d71963ce162925facd5
IVainqueur <ishimvainqueur@gmail.com>
2022-08-14 23:27:00 +0200

    auto-commit-3428a2e899c74176b50d0d4f38776ea1

--- methods.py
+++ methods.py
@@ -128,6 +128,7 @@ def push(ct, dir, branch, interval, beforemethod=None):
         # print("--> Set Branch to {br}".format(br=colorcode(branch, "green")))
         subprocess.call(["git", "-C", dir, "push", "origin", branch], stdout=subprocess.DEVNULL)
         print("--> Pushed to {br}".format(br=colorcode(branch, "green")))
+        clear()
     except PauseException:
         pass
     except Exception as e:
