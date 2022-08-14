5c946372a6efbaf6206457b77526db1e75c977d2
IVainqueur <ishimvainqueur@gmail.com>
2022-08-15 00:11:06 +0200

    auto-commit-1cc4039157dc48389b89955b310f2a90

--- methods.py
+++ methods.py
@@ -195,7 +195,7 @@ def listenForKeys(key):
         return
     # If the key is not a single key command
     key_parsed = key.rsplit(" ")
-    command = OTHER_CMDS.get(key.rsplit(" ")[0], None)
+    command = OTHER_CMDS.get(key_parsed[0], None)
     if command:
         print(f"Calling {command} with parameters {key_parsed[1:]}")
         command(key_parsed[1:])
